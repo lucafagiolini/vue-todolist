c849fdf2482a1d1fa14cd5ae286d3182113fd215
Luca <lucafagiolini@live.it>
2024-02-20 18:26:48 +0100

    array of objects/to do list created

--- js/script.js
+++ js/script.js
@@ -4,6 +4,15 @@ createApp({
     data() {
         return {
             // tutti i nostri dati
+            mainTitle: "to do list",
+
+            newTodo: [
+                { title: "Fare la spesa", completed: false },
+                { title: "Fare il bucato", completed: false },
+                { title: "Fare il letto", completed: false },
+                { title: "Fare la doccia", completed: false },
+                { title: "Fare la cena", completed: false },
+            ],
         }
     }
 }).mount("#app");
